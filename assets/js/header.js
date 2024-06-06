dd91b4cb1ab2c13762e8911a35d3ba30c0d358d3
Jamshid <jamshid_tojiboyev@mail.ru>
2024-06-06 23:37:13 +0500

    added

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,15 +1,28 @@
 const menuBtn = document.querySelector('.menu-btn'),
+      menu = document.querySelector('.menu'),
       header = document.querySelector('header'),
       body = document.querySelector('body');
       list = document.querySelector('.menu .nav-list-item');
 
 menuBtn.addEventListener('click', () => {
-    header.classList.toggle('showMenu');
-    body.classList.toggle('bodyStopScroll')
+    menu.classList.toggle('showMenu');
+    menuBtn.classList.toggle('clickedBtn');
+    body.classList.toggle('bodyStopScroll');
+    header.classList.toggle('noShadow')
 });
 
 
 list.addEventListener('click', () => {
     list.classList.toggle('showNavItem');
 });
+
+$(document).ready(function () {
+    $(window).on('scroll', function () {
+      if ($(window).scrollTop()) {
+        $('header').addClass('headerSticy')
+      } else {
+        $('header').removeClass('headerSticy')
+      }
+    });
+  });
 
